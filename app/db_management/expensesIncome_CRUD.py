ccd37a238be6f8419f2ce919b4783c8eaa4a4f74
הנדסת תוכנה <huser@sem.local>
2024-05-29 22:02:32 +0300

    sucsses error

--- app/db_management/expensesIncome_CRUD.py
+++ app/db_management/expensesIncome_CRUD.py
@@ -3,12 +3,16 @@ from app.models.Expenses_incomes import Expenses_Income
 
 
 async def get_all():
-    Expenses_Income.find()
-    return [Expenses_Income(**expenses_income) for expenses_income in Expenses_Income.find()]
+    print("befor הקריאה לDB")
+    x = list(expensesAndIncomeCollection.find())
+    [s.pop('_id') for s in x]
+    print("in func get_all")
+    return x
+    # return [Expenses_Income(**expenses_income) for expenses_income in Expenses_Income.find()]
 
 
 async def add_expenses(list: Expenses_Income):
     expensesAndIncomeCollection.insert_one(list.dict)
     return 1
-async  def update(expenses_Income: Expenses_Income):
-    
+# async  def update(expenses_Income: Expenses_Income):
+#
