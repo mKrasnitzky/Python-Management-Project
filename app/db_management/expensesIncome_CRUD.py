a22e22aa9c4bf9c80860bc4bab08a4ffc120d31e
הנדסת תוכנה <huser@sem.local>
2024-05-30 14:05:39 +0300

    finish function CRUD

--- app/db_management/expensesIncome_CRUD.py
+++ app/db_management/expensesIncome_CRUD.py
@@ -3,16 +3,31 @@ from app.models.Expenses_incomes import Expenses_Income
 
 
 async def get_all():
-    print("befor הקריאה לDB")
     x = list(expensesAndIncomeCollection.find())
     [s.pop('_id') for s in x]
-    print("in func get_all")
     return x
     # return [Expenses_Income(**expenses_income) for expenses_income in Expenses_Income.find()]
+
+
+async def get(id: int):
+    expensesAndIncomeCollection.find({
+        "id": id
+    })
 
 
 async def add_expenses(list: Expenses_Income):
     expensesAndIncomeCollection.insert_one(list.dict)
     return 1
-# async  def update(expenses_Income: Expenses_Income):
-#
+
+
+async def update(id: int, name: String):
+    expensesAndIncomeCollection.insert_one({
+        "id": id,
+        "name": name
+    })
+
+
+async def delete(ei: Expenses_Income):
+    expensesAndIncomeCollection.delete_one(ei)
+    return "the object delete"
+
